c362fe25afa4ee77f4fd1a80a35efdaa82fedf0f
shiar18 <arshisingh1808@gmail.com>
2024-01-11 23:14:47 +0530

    Changing line 27

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,8 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      )
+      home: const MyHomePage(title: 'Arshi LOL'),
     );
   }
 }
